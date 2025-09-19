b5b7570407e4b1ed039118484f9ed554e7651aa4
aflahkm636 <aflahkm007@gmail.com>
2025-09-19 14:51:34 +0530

    day 3 task completed

--- ADOdemo/Program.cs
+++ ADOdemo/Program.cs
@@ -6,7 +6,7 @@ using System.Data.SqlClient;
 class Program
 
 {
-    [Obsolete]
+
     static void Main(string[] args)
     {
         //string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True;";
@@ -67,47 +67,144 @@ class Program
 
         ///day2
 
-        string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True;";
-        using(SqlConnection conn =new SqlConnection(connStr))
+        //string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True;";
+        //using(SqlConnection conn =new SqlConnection(connStr))
+        //{
+        //    conn.Open();
+
+        //    SqlDataAdapter adapter = new SqlDataAdapter("select * from students",conn);
+
+        //    SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+        //    DataSet dataSet = new DataSet();
+        //    adapter.Fill(dataSet,"students");
+
+
+        //DataRow newrow = dataSet.Tables["students"].NewRow();
+        //newrow["name"] = "allli";
+        //newrow["Age"] = 35;
+        //dataSet.Tables["students"].Rows.Add(newrow);
+
+        //DataRow uprow = dataSet.Tables["students"].Rows[0];
+        //uprow["age"] = 11;
+
+        //  DataTable dt = dataSet.Tables["students"];
+        //  DataView veiw = new DataView(dt);
+        //  veiw.RowFilter = "Age>20";
+        //foreach(DataRowView row in veiw)
+        //  {
+        //      Console.WriteLine($"name:{row["Name"]},Age:{row["Age"]}");
+
+        //  }
+
+        //foreach (DataRow row in dataSet.Tables["students"].Rows)
+        //{
+        //    Console.WriteLine($"name:{row["Name"]},Age:{row["Age"]}");
+        //}
+
+        //DataRow deleterow = dataSet.Tables["students"].Rows[6];
+        //deleterow.Delete();
+        //adapter.Update(dataSet, "students");
+
+
+        //day3
+
+        string connstr = @"Data Source=(localdb)\MSSQLLocalDB ;Initial Catalog= SchoolDB ;Integrated security = True;";
+
+        //using (SqlConnection conn = new SqlConnection(connstr))
+        //{
+           
+        //    SqlCommand cmd = new SqlCommand("GetStudentById", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+
+        //    cmd.Parameters.AddWithValue("@StudentId" ,1);
+
+        //    conn.Open();
+
+        //    using (SqlDataReader reader = cmd.ExecuteReader()) {
+        //        while (reader.Read())
+        //        {
+        //            Console.WriteLine($"name:{reader["Name"]},Age:{reader["Age"]}");
+        //        }
+        //    }
+        //}
+
+        //using(SqlConnection conn =new SqlConnection(connstr))
+        //{
+        //    SqlCommand cmd=new SqlCommand("AddStudent",conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    cmd.Parameters.AddWithValue("@Name", "qwerty");
+        //    cmd.Parameters.AddWithValue("@Age", 32);
+        //    cmd.Parameters.AddWithValue("@DOB", "2002-1-12");
+        //    cmd.Parameters.AddWithValue("@Grade", "A");
+
+
+        //    conn.Open();
+
+        //    int rowsAffected=cmd.ExecuteNonQuery();
+        //    Console.WriteLine($"{rowsAffected} rows inserted");
+
+        //}
+
+        //using (SqlConnection conn = new SqlConnection(connstr)) 
+        //{
+        //    SqlCommand updateCmd = new SqlCommand("UpdateAge", conn);
+        //    updateCmd.CommandType = CommandType.StoredProcedure;
+        //    updateCmd.Parameters.AddWithValue("@StudentId",4);
+        //    updateCmd.Parameters.AddWithValue("@Age", 22);
+
+        //    conn.Open( );
+
+        //    int rowsAffected=updateCmd.ExecuteNonQuery();
+        //    Console.WriteLine($"{rowsAffected} reos updated");
+        //}
+
+        //using (SqlConnection conn = new SqlConnection(connstr)) 
+        //{
+        //    SqlCommand deletecmd = new SqlCommand("deleteStudent", conn);
+        //    deletecmd.CommandType = CommandType.StoredProcedure;
+        //    deletecmd.Parameters.AddWithValue("@studentId", 3);
+
+        //    conn.Open( );
+
+        //    int rowsAffected=deletecmd.ExecuteNonQuery();
+        //    Console.WriteLine($"{rowsAffected} rows deleted");
+        //}
+
+
+        using(SqlConnection conn =new SqlConnection(connstr))
         {
             conn.Open();
 
-            SqlDataAdapter adapter = new SqlDataAdapter("select * from students",conn);
+            SqlTransaction transaction = conn.BeginTransaction();
 
-            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
-
-            DataSet dataSet = new DataSet();
-            adapter.Fill(dataSet,"students");
-
-
-            //DataRow newrow = dataSet.Tables["students"].NewRow();
-            //newrow["name"] = "allli";
-            //newrow["Age"] = 35;
-            //dataSet.Tables["students"].Rows.Add(newrow);
-
-            //DataRow uprow = dataSet.Tables["students"].Rows[0];
-            //uprow["age"] = 11;
-
-            DataTable dt = dataSet.Tables["students"];
-            DataView veiw = new DataView(dt);
-            veiw.RowFilter = "Age>20";
-          foreach(DataRowView row in veiw)
+            try
             {
-                Console.WriteLine($"name:{row["Name"]},Age:{row["Age"]}");
+                SqlCommand insertcmd = new SqlCommand("insert into students(Name,Age,DOB,Grade) values(@Name,@Age,@DOB,@Grade)",
+                    conn, transaction);
+                insertcmd.Parameters.AddWithValue("@Name", "ZXCVB");
+                insertcmd.Parameters.AddWithValue("@Age ", 20);
+                insertcmd.Parameters.AddWithValue("@DOB","2003-3-21");
+                insertcmd.Parameters.AddWithValue("@Grade", "B");
+                insertcmd.ExecuteNonQuery();
+               
+                SqlCommand updatecmd = new SqlCommand("update students set Name=@Name,Age=@Age where StudentId=@StudentId", conn, transaction);
+                updatecmd.Parameters.AddWithValue("@StudentId", 1);
+                updatecmd.Parameters.AddWithValue("@Name", "FASi");
+                updatecmd.Parameters.AddWithValue("@Age", 2);
+                updatecmd.ExecuteNonQuery();
 
+                transaction.Commit();
+                Console.WriteLine("transaction commited");
+                
             }
-
-            //foreach (DataRow row in dataSet.Tables["students"].Rows)
-            //{
-            //    Console.WriteLine($"name:{row["Name"]},Age:{row["Age"]}");
-            //}
-
-            //DataRow deleterow = dataSet.Tables["students"].Rows[6];
-            //deleterow.Delete();
-            adapter.Update(dataSet, "students");
-
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine("transaction rooled back.error:"+ex.Message);
+            }
         }
 
-
     }
-}
+    }
+
